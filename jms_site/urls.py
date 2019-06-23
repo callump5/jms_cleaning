2c4aa2355248706cbd3a323421291de71213b69b
Callum <clpullinger@gmail.com>
2019-06-23 19:47:00 +0100

    Content Update

--- jms_site/urls.py
+++ jms_site/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from .views import get_index, get_robots, get_sitemap, get_contact, get_service, get_services, get_testimonials, get_gallery
+from .views import get_index, get_robots, get_about, get_sitemap, get_contact, get_service, get_services, get_testimonials, get_gallery
 urlpatterns = [
 
 
@@ -17,6 +17,9 @@ urlpatterns = [
     url(r'^end-of-tenancy-cleaning/$', get_index),
     url(r'^end-of-tenancy-cleaning-essex/$', get_index),
     url(r'^end-of-tenancy-cleaning-southend/$', get_index),
+
+    # About Page
+    url(r'^about/$', get_about, name='about'),
 
     # Services Page
     url(r'^services/$', get_services, name='services'),
