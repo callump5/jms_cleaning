b53fee78575f283c256f51a9f7a171fa99c1e084
Callum <clpullinger@gmail.com>
2019-08-19 14:35:52 +0100

    Final Changes

--- jms_site/views.py
+++ jms_site/views.py
@@ -255,7 +255,7 @@ def get_contact(request):
 def get_sitemap(request):
 
     services = Service.objects.all()
-    date = datetime.now()
+    date = datetime.today().strftime('%Y-%m-%d')
     args = {
         'date': date,
         'services': services
